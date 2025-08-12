6c3345df84f062369731c4c35d134ef8b84d915d
pablenicolas <95651314+pablenicolas@users.noreply.github.com>
2025-08-12 11:11:42 -0300

    Actualización: versión solo online + equipo nuevo

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 
-// Basic enhancements
+// Prefill professional from querystring (?prof=rodrigo|natalia)
 (function(){
   const params = new URLSearchParams(location.search);
   const prof = params.get('prof');
   const select = document.getElementById('profesional');
   if (prof && select){
     for (const opt of select.options){
-      if (opt.value.toLowerCase().includes(prof)){
+      if (opt.text.toLowerCase().includes(prof)){
         opt.selected = true;
         break;
       }
